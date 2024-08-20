e729e289dcdd32e64c1368369f52432b3bd58407
Petter Andersson <petter.andersson@helsingborg.se>
2024-08-20 09:47:56 +0200

    fix script

--- scripts/stratsys.sh
+++ scripts/stratsys.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 if [ -z ${STRATSYS_PATH} ]; then
     echo "Missing env variable STRATSYS_PATH"; exit 1
@@ -18,10 +19,12 @@ fi
 if [ -z ${TYPESENSE_PATH} ]; then
     echo "Missing env variable TYPESENSE_PATH"; exit 1
 fi
+cd ${SCRIPT_DIR}
+
 TMPFILE=$(mktemp)
 
 # Retreive and transform Stratsys export
-php router.php \
+php ../router.php \
     --source ${STRATSYS_PATH} \
     --authpath ${STRATSYS_AUTH} \
     --authclientid ${STRATSYS_CLIENTID} \
@@ -34,13 +37,20 @@ php router.php \
 if [ $? -ne 0 ]; then
     echo "FAILED to transform stratsys request to file ${TMPFILE}"
 else
+    # Clear collection
+    echo "Deleting documents"
+    curl ${TYPESENSE_PATH}?filter_by=@type:Article -X DELETE -H "x-typesense-api-key: ${TYPESENSE_APIKEY}"
+
+    if [ $? -ne 0 ]; then
+        echo "FAILED to delete documents"
+    fi
+
     # POST result to typesense
-    curl ${TYPESENSE_PATH} -X POST --data-binary @${TMPFILE} -H "Content-Type: text/plain" -H "x-typesense-api-key: ${TYPESENSE_APIKEY}"
+    echo "Creating documents"
+    curl ${TYPESENSE_PATH}/import?action=create -X POST --data-binary @${TMPFILE} -H "Content-Type: text/plain" -H "x-typesense-api-key: ${TYPESENSE_APIKEY}"
 
     if [ $? -ne 0 ]; then
         echo "FAILED to upload to ${TYPESENSE_PATH}"
-    else
-        echo "Upload SUCCEEDED"
     fi
 fi
 # Remove temp file
