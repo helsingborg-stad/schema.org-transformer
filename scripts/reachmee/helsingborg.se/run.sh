24e4637a3137295b5b1f276121e4583f158dfb5c
Petter Andersson <petter.andersson@helsingborg.se>
2024-08-21 11:00:25 +0200

    fix script

---
+++ scripts/reachmee/helsingborg.se/run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+if [ -z ${REACHMEE_HELSINGBORG_PATH} ]; then
+    echo "Missing env variable STRATSYS_INNOVATION_PATH"; exit 1
+fi
+if [ -z ${TYPESENSE_APIKEY} ]; then
+    echo "Missing env variable TYPESENSE_APIKEY"; exit 1
+fi
+if [ -z ${TYPESENSE_BASE_PATH} ]; then
+    echo "Missing env variable TYPESENSE_BASE_PATH"; exit 1
+fi
+cd ${SCRIPT_DIR}
+
+TMPFILE=$(mktemp)
+TYPESENSE_PATH=${TYPESENSE_BASE_PATH}/collections/jobpostings/documents
+
+# Retreive and transform Stratsys export
+php ../../../router.php \
+    --source ${REACHMEE_HELSINGBORG_PATH} \
+    --transform jobposting \
+    --outputformat jsonl \
+    --output ${TMPFILE}
+
+if [ $? -ne 0 ]; then
+    echo "FAILED to transform request to file ${TMPFILE}"
+else
+    # Clear collection
+    echo "Deleting documents"
+    curl ${TYPESENSE_PATH}?filter_by=@type:JobPosting -X DELETE -H "x-typesense-api-key: ${TYPESENSE_APIKEY}"
+
+    if [ $? -ne 0 ]; then
+        echo "FAILED to delete documents"
+    fi
+
+    # POST result to typesense
+    echo "Creating documents"
+    curl ${TYPESENSE_PATH}/import?action=create -X POST --data-binary @${TMPFILE} -H "Content-Type: text/plain" -H "x-typesense-api-key: ${TYPESENSE_APIKEY}"
+
+    if [ $? -ne 0 ]; then
+        echo "FAILED to upload document"
+    fi
+fi
+# Remove temp file
+rm -f ${TMPFILE}
--- scripts/stratsys/innovation.helsingborg.se/run.sh
+++ scripts/stratsys/innovation.helsingborg.se/run.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-if [ -z ${STRATSYS_PATH} ]; then
-    echo "Missing env variable STRATSYS_PATH"; exit 1
+if [ -z ${STRATSYS_INNOVATION_PATH} ]; then
+    echo "Missing env variable STRATSYS_INNOVATION_PATH"; exit 1
 fi
-if [ -z ${STRATSYS_AUTH} ]; then
-    echo "Missing env variable STRATSYS_AUTH"; exit 1
+if [ -z ${STRATSYS_INNOVATION_AUTH} ]; then
+    echo "Missing env variable STRATSYS_INNOVATION_AUTH"; exit 1
 fi
-if [ -z ${STRATSYS_CLIENTID} ]; then
-    echo "Missing env variable STRATSYS_CLIENTID"; exit 1
+if [ -z ${STRATSYS_INNOVATION_CLIENTID} ]; then
+    echo "Missing env variable STRATSYS_INNOVATION_CLIENTID"; exit 1
 fi
-if [ -z ${STRATSYS_CLIENTSECRET} ]; then
+if [ -z ${STRATSYS_INNOVATION_CLIENTSECRET} ]; then
     echo "Missing env variable STRATSYS_CLIENTSECRET"; exit 1
 fi
 if [ -z ${TYPESENSE_APIKEY} ]; then
     echo "Missing env variable TYPESENSE_APIKEY"; exit 1
 fi
-if [ -z ${TYPESENSE_PATH} ]; then
-    echo "Missing env variable TYPESENSE_PATH"; exit 1
+if [ -z ${TYPESENSE_BASE_PATH} ]; then
+    echo "Missing env variable TYPESENSE_BASE_PATH"; exit 1
 fi
 cd ${SCRIPT_DIR}
 
 TMPFILE=$(mktemp)
+TYPESENSE_PATH=${TYPESENSE_BASE_PATH}/collections/stratsys/documents
 
 # Retreive and transform Stratsys export
-php ../router.php \
-    --source ${STRATSYS_PATH} \
-    --authpath ${STRATSYS_AUTH} \
-    --authclientid ${STRATSYS_CLIENTID} \
-    --authclientsecret ${STRATSYS_CLIENTSECRET} \
-    --authscope 'exportview.read' \
+php ../../../router.php \
+    --source ${STRATSYS_INNOVATION_PATH} \
+    --authpath ${STRATSYS_INNOVATION_AUTH} \
+    --authclientid ${STRATSYS_INNOVATION_CLIENTID} \
+    --authclientsecret ${STRATSYS_INNOVATION_CLIENTSECRET} \
+    --authscope exportview.read \
     --transform stratsys \
     --outputformat jsonl \
     --output ${TMPFILE}
 
 if [ $? -ne 0 ]; then
-    echo "FAILED to transform stratsys request to file ${TMPFILE}"
+    echo "FAILED to transform request to file ${TMPFILE}"
 else
     # Clear collection
     echo "Deleting documents"
@@ -50,7 +51,7 @@ else
     curl ${TYPESENSE_PATH}/import?action=create -X POST --data-binary @${TMPFILE} -H "Content-Type: text/plain" -H "x-typesense-api-key: ${TYPESENSE_APIKEY}"
 
     if [ $? -ne 0 ]; then
-        echo "FAILED to upload to ${TYPESENSE_PATH}"
+        echo "FAILED to upload document"
     fi
 fi
 # Remove temp file
