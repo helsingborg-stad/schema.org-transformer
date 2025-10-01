129970a0e8a7a28dabd386ba94f94afcff473742
Joakim Larsson <mimmertim@gmail.com>
2025-10-01 20:07:16 +0200

    event::eventSchedule is expanded from weekly recurring occasions

--- src/Transforms/WPHeadLessEvents/Mappers/MapEventSchedule.php
+++ src/Transforms/WPHeadLessEvents/Mappers/MapEventSchedule.php
@@ -8,9 +8,34 @@ use DateTime;
 use Municipio\Schema\Event;
 use Municipio\Schema\Schema;
 use SchemaTransformer\Transforms\WPHeadLessEvents\Mappers\AbstractWPHeadlessEventMapper;
+use DateInterval;
+use DateMalformedStringException;
+use DatePeriod;
+use Exception;
+use TypeError;
 
 class MapEventSchedule extends AbstractWPHeadlessEventMapper
 {
+    private $weekDayMap = [
+        'https://schema.org/Monday'    => 1,
+        'https://schema.org/Tuesday'   => 2,
+        'https://schema.org/Wednesday' => 3,
+        'https://schema.org/Thursday'  => 4,
+        'https://schema.org/Friday'    => 5,
+        'https://schema.org/Saturday'  => 6,
+        'https://schema.org/Sunday'    => 7,
+    ];
+
+    private array $weekdayNames = [
+                1 => 'monday',
+                2 => 'tuesday',
+                3 => 'wednesday',
+                4 => 'thursday',
+                5 => 'friday',
+                6 => 'saturday',
+                7 => 'sunday',
+
+    ];
     public function __construct()
     {
         parent::__construct();
@@ -18,29 +43,94 @@ class MapEventSchedule extends AbstractWPHeadlessEventMapper
 
     public function map(Event $event, array $data): Event
     {
-        return $event->eventSchedule(
-            array_values(
-                array_filter(
-                    array_map(
+        $schedules = array_values(
+            array_filter(
+                array_merge(
+                    ...array_map(
                         fn ($occasion) =>
-                            Schema::schedule()
-                                ->startDate(OccasionHelper::tryMapDate($occasion['date'] ?? ''))
-                                ->endDate(OccasionHelper::tryMapDate($occasion['untilDate'] ?? ''))
-                                ->startTime($occasion['startTime'] ?? null)
-                                ->endTime($occasion['endTime'] ?? null)
-                                ->byDay($occasion['weekDays'] ?? null),
+                            $this->tryMapMonthlyOccasionToSchedules($occasion)
+                            ?? $this->tryMapWeeklyOccasionToSchedules($occasion)
+                            ?? $this->mapOccassionToSchedules($occasion),
                         $data['acf']['occasions'] ?? []
                     )
                 )
             )
         );
+        usort($schedules, fn ($schedule1, $schedule2) => $schedule1->getProperty('startDate') <=> $schedule2->getProperty('startDate'));
+        return $event->eventSchedule($schedules);
+    }
+
+    public function mapOccassionToSchedules($occasion): ?array /* of Schema::schedule() */
+    {
+        return [
+            Schema::schedule()
+                ->startDate($this->tryParseDate($occasion['date'])?->format('Y-m-d'))
+                ->endDate($this->tryParseDate($occasion['untilDate'])?->format('Y-m-d'))
+                ->startTime($occasion['startTime'] ?? null)
+                ->endTime($occasion['endTime'] ?? null)
+                ->description($occasion['description'] ?? null)
+        ];
+    }
+
+    public function tryMapWeeklyOccasionToSchedules($occasion): ?array /* of Schema::schedule() */
+    {
+        if ($occasion['repeat'] !== 'byWeek') {
+            return null;
+        }
+        if (empty($occasion['date']) || empty($occasion['untilDate']) || empty($occasion['weekDays'])) {
+            return null;
+        }
+        return array_merge(
+            ...array_map(
+                fn ($weekDay) => array_map(
+                    fn ($date) => Schema::schedule()
+                        ->startDate($date->format('Y-m-d'))
+                        ->endDate($date->format('Y-m-d'))
+                        ->startTime($occasion['startTime'] ?? null)
+                        ->endTime($occasion['endTime'] ?? null)
+                        ->description($occasion['description'] ?? null),
+                    $this->expandWeeklyDates(
+                        $this->tryParseDate($occasion['date'] ?? ''),
+                        $this->tryParseDate($occasion['untilDate'] ?? ''),
+                        $this->weekDayMap[$weekDay] ?? null,
+                        (int)($occasion['weeksInterval'] ?? 1)
+                    )
+                ),
+                $occasion['weekDays'] ?? []
+            )
+        );
+    }
+
+    public function tryMapMonthlyOccasionToSchedules($occasion): ?array /* of Schema::schedule() */
+    {
+        // TODO: understand and implement monthly recurrence
+        return null;
+    }
+
+    public function expandWeeklyDates(
+        DateTime $start, // assumed date only
+        DateTime $end, // assumed date only
+        int $dayOfWeek,
+        int $weekInterval = 1
+    ): array {
+        // adjust start to next occurence of weekday
+        while ((int)$start->format('N') !== $dayOfWeek) {
+            $start->modify('next ' . $this->weekdayNames[$dayOfWeek]);
+        }
+        $data     = [];
+        $interval = new DateInterval('P' . $weekInterval . 'W');
+        $period   = new DatePeriod($start, $interval, $end, DatePeriod::INCLUDE_END_DATE);
+        foreach ($period as $date) {
+            $data[] = (clone $date);
+        }
+        return $data;
     }
 
     private function tryParseDate($date): ?DateTime
     {
         try {
             return new DateTime($date);
-        } catch (\Exception) {
+        } catch (DateMalformedStringException | Exception | TypeError $e) {
             return null;
         }
     }
--- src/Transforms/WPHeadLessEvents/Mappers/Tests/MapEventScheduleTest.php
+++ src/Transforms/WPHeadLessEvents/Mappers/Tests/MapEventScheduleTest.php
@@ -11,6 +11,7 @@ use Municipio\Schema\Schema;
 use SchemaTransformer\Transforms\WPHeadLessEvents\Mappers\MapEventSchedule;
 use SchemaTransformer\Transforms\WPHeadLessEvents\WPHeadlessEventTransform;
 use SchemaTransformer\Transforms\WPHeadLessEvents\Mappers\Tests\TestHelper;
+use DateTime;
 
 #[CoversClass(MapEventSchedule::class)]
 final class MapEventScheduleTest extends TestCase
@@ -25,16 +26,16 @@ final class MapEventScheduleTest extends TestCase
                     "occasions": [
                         {
                             "repeat": "byWeek",
-                            "weeksInterval": 3,
+                            "weeksInterval": 2,
                             "monthsInterval": 1,
                             "weekDays": [
-                                "https://schema.org/Monday",
-                                "https://schema.org/Wednesday"
+                                "https://schema.org/Wednesday",
+                                "https://schema.org/Thursday"
                             ],
                             "monthDay": "dag",
                             "monthDayNumber": 1,
-                            "date": "20250818",
-                            "untilDate": "20251114",
+                            "date": "20251001",
+                            "untilDate": "20251031",
                             "startTime": "12:00:00",
                             "endTime": "15:00:00",
                             "url": ""
@@ -44,27 +45,99 @@ final class MapEventScheduleTest extends TestCase
             }',
             Schema::event()->eventSchedule([
                 Schema::schedule()
-                    ->startDate('2025-08-18')
-                    ->endDate('2025-11-14')
+                    ->startDate('2025-10-01')
+                    ->endDate('2025-10-01')
+                    ->startTime('12:00:00')
+                    ->endTime('15:00:00'),
+                Schema::schedule()
+                    ->startDate('2025-10-02')
+                    ->endDate('2025-10-02')
+                    ->startTime('12:00:00')
+                    ->endTime('15:00:00'),
+
+                Schema::schedule()
+                    ->startDate('2025-10-15')
+                    ->endDate('2025-10-15')
+                    ->startTime('12:00:00')
+                    ->endTime('15:00:00'),
+                Schema::schedule()
+                    ->startDate('2025-10-16')
+                    ->endDate('2025-10-16')
+                    ->startTime('12:00:00')
+                    ->endTime('15:00:00'),
+                Schema::schedule()
+                    ->startDate('2025-10-29')
+                    ->endDate('2025-10-29')
+                    ->startTime('12:00:00')
+                    ->endTime('15:00:00'),
+                Schema::schedule()
+                    ->startDate('2025-10-30')
+                    ->endDate('2025-10-30')
                     ->startTime('12:00:00')
                     ->endTime('15:00:00')
-                    ->byDay([
-                        'https://schema.org/Monday',
-                        'https://schema.org/Wednesday',
-                    ])
+
             ])
         );
     }
 
-    #[TestDox('event::name(null) when acf.name is missing')]
-    public function testMissing()
+    #[TestDox('expandWeeklyDates generates correct dates for weekly events')]
+    public function testExpandWeeklyDates()
     {
-        (new TestHelper())->expectMapperToConvertSourceTo(
-            new MapEventSchedule(new WPHeadlessEventTransform('hl')),
-            '{
-                "id": 123
-            }',
-            Schema::event()->eventSchedule([])
+        $events = (new MapEventSchedule())->expandWeeklyDates(
+            new DateTime('2025-10-01'),
+            new DateTime('2025-10-31'),
+            2, // tuesdays
+            1  // every week
+        );
+        $this->assertEquals(
+            [
+            new DateTime('2025-10-07'),
+            new DateTime('2025-10-14'),
+            new DateTime('2025-10-21'),
+            new DateTime('2025-10-28')
+            ],
+            $events
+        );
+    }
+
+    #[TestDox('expandWeeklyDates generates correct dates for weekly events over new years eve')]
+    public function testExpandWeeklyDatesOverNewYear()
+    {
+        $events = (new MapEventSchedule())->expandWeeklyDates(
+            new DateTime('2025-12-27'),
+            new DateTime('2026-02-07'),
+            6, // saturdays
+            2  // every 2:nd week
+        );
+        $this->assertEquals(
+            [
+            new DateTime('2025-12-27'),
+            new DateTime('2026-01-10'),
+            new DateTime('2026-01-24'),
+            new DateTime('2026-02-07')
+            ],
+            $events
+        );
+    }
+
+    #[TestDox('expandWeeklyDates tolerates start and end dates that are not on the correct weekday')]
+    public function testExpandWeeklyDatesStartAndEndAreWrongWeekdays()
+    {
+        $events = (new MapEventSchedule())->expandWeeklyDates(
+            new DateTime('2025-10-01'),
+            new DateTime('2025-10-31'),
+            4, // thursdays
+            1  // every week
+        );
+        $this->assertEquals(
+            [
+                new DateTime('2025-10-02'),
+                new DateTime('2025-10-09'),
+                new DateTime('2025-10-16'),
+                new DateTime('2025-10-23'),
+                new DateTime('2025-10-30')
+            ],
+            $events
         );
     }
 }
