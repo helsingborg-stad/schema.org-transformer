c1c5cdb5ff29dcbc32535b3700ad2c5e61acd655
Joakim Larsson <mimmertim@gmail.com>
2025-11-10 09:36:51 +0100

    fix: locking/syncronization school imports

--- scripts/elementary-school/run.sh
+++ scripts/elementary-school/run.sh
@@ -11,44 +11,48 @@ if [ $? -ne 0 ]; then
 fi
 cd ${SCRIPT_DIR}
 
-# Retrieve and transform elementary school data
-php ../../router.php \
-    --source ${ELEMENTARY_SCHOOL_API_URL} \
-    --transform elementary_school \
-    --outputformat jsonl \
-    --output ${TMPFILE} \
-    --paginator wordpress \
-    --idprefix R \
-    --typesense_apikey "${TYPESENSE_APIKEY}" \
-    --typesense_host "${TYPESENSE_HOST}" \
-    --typesense_port "${TYPESENSE_PORT}"
+(
+    flock -n 9 || exit 1
 
-if [ $? -ne 0 ]; then
-    echo "FAILED to transform request"
-else
-    # Clear collection
-    echo "Deleting documents"
-    curl ${TYPESENSE_PATH}?filter_by=@type:ElementarySchool -X DELETE -H "x-typesense-api-key: ${TYPESENSE_APIKEY}"
+    # Retrieve and transform elementary school data
+    php ../../router.php \
+        --source ${ELEMENTARY_SCHOOL_API_URL} \
+        --transform elementary_school \
+        --outputformat jsonl \
+        --output ${TMPFILE} \
+        --paginator wordpress \
+        --idprefix R \
+        --typesense_apikey "${TYPESENSE_APIKEY}" \
+        --typesense_host "${TYPESENSE_HOST}" \
+        --typesense_port "${TYPESENSE_PORT}"
 
     if [ $? -ne 0 ]; then
-        echo "FAILED to delete documents"
+        echo "FAILED to transform request"
+    else
+        # Clear collection
+        echo "Deleting documents"
+        curl ${TYPESENSE_PATH}?filter_by=@type:ElementarySchool -X DELETE -H "x-typesense-api-key: ${TYPESENSE_APIKEY}"
+
+        if [ $? -ne 0 ]; then
+            echo "FAILED to delete documents"
+        fi
+
+        # POST result to typesense
+        echo "Creating documents"
+        curl ${TYPESENSE_PATH}/import?action=create -X POST --data-binary @${TMPFILE} -H "Content-Type: text/plain" -H "x-typesense-api-key: ${TYPESENSE_APIKEY}"
+
+        if [ $? -ne 0 ]; then
+            echo "FAILED to upload document"
+        fi
+
+        # Clear typesense cache
+        echo "Clearing Typesense cache"
+        curl -H "X-TYPESENSE-API-KEY: ${TYPESENSE_APIKEY}" -X POST ${TYPESENSE_BASE_PATH}/operations/cache/clear
+
+        if [ $? -ne 0 ]; then
+            echo "FAILED to clear Typesense cache"
+        fi
     fi
-
-    # POST result to typesense
-    echo "Creating documents"
-    curl ${TYPESENSE_PATH}/import?action=create -X POST --data-binary @${TMPFILE} -H "Content-Type: text/plain" -H "x-typesense-api-key: ${TYPESENSE_APIKEY}"
-
-    if [ $? -ne 0 ]; then
-        echo "FAILED to upload document"
-    fi
-
-    # Clear typesense cache
-    echo "Clearing Typesense cache"
-    curl -H "X-TYPESENSE-API-KEY: ${TYPESENSE_APIKEY}" -X POST ${TYPESENSE_BASE_PATH}/operations/cache/clear
-
-    if [ $? -ne 0 ]; then
-        echo "FAILED to clear Typesense cache"
-    fi
-fi
-# Remove temp file
-rm -f ${TMPFILE}
+    # Remove temp file
+    rm -f ${TMPFILE}
+) 9>/tmp/elementary-school.lock
