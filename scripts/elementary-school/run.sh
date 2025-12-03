ee488f43bb01a443032f296f1a003ceab0c568a7
Thor Brink <thor.brink@helsingborg.se>
2025-12-03 09:21:25 +0000

    fix: add monitoring URL calls in run scripts

--- scripts/elementary-school/run.sh
+++ scripts/elementary-school/run.sh
@@ -53,6 +53,9 @@ cd ${SCRIPT_DIR}
 
         if [ $? -ne 0 ]; then
             echo "FAILED to clear Typesense cache"
+        else
+            # Call monitoring url if set
+            if [ ! -z ${ELEMENTARY_SCHOOL_MONITOR_URL} ]; then curl -s ${ELEMENTARY_SCHOOL_MONITOR_URL} >/dev/null; fi
         fi
     fi
     # Remove temp file
