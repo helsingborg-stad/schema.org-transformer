a7835764cc5612e7d0d5df897b0bbd3172c369d2
Joakim Larsson <mimmertim@gmail.com>
2026-03-27 12:59:25 +0000

    feat: 1 month retention

--- src/Transforms/Event/AxiellEvents/AxiellEventTransform.php
+++ src/Transforms/Event/AxiellEvents/AxiellEventTransform.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SchemaTransformer\Transforms\Event\AxiellEvents;
 
+use DateTime;
 use Municipio\Schema\Schema;
 use SchemaTransformer\Transforms\TransformBase;
 use SchemaTransformer\Interfaces\AbstractDataTransform;
@@ -38,17 +39,21 @@ class AxiellEventTransform extends TransformBase implements AbstractDataTransfor
 
     public function preprocessData(array $data): array
     {
+        // NOTE: HARDCODED RETENTION DATE FOR EVENTS, SHOULD BE REPLACED WITH A CONFIGURABLE OPTION
+        $retentionDate = (new DateTime())->sub(new \DateInterval('P1M'))->format('Y-m-d'); // Subtract 1 month from the current date
+
         // Implement any necessary preprocessing steps here
         return array_filter(
             // project $.hits[*].event
             array_map(fn($item) => $item['event'] ?? [], $data['hits'] ?? []),
-            // filter out events that have any of the exclude tags
-            fn($item) => count(
+            // filter out events that have any of the exclude tags or have an end date before the retention date
+            fn($item) =>
+            (count(
                 array_intersect(
                     array_map('strtolower', $item['tags'] ?? []),
                     $this->excludeTags
                 )
-            ) === 0
+            ) === 0) && ($item['endDate'] ?? '') >= $retentionDate
         );
     }
 
