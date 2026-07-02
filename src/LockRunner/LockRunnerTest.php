80a8bd8afe8d3a3577a3cc85f8777994e247609a
Thor Brink <thor.brink@helsingborg.se>
2026-07-02 06:09:33 +0000

    fix: tests for LockRunner

--- src/LockRunner/LockRunnerTest.php
+++ src/LockRunner/LockRunnerTest.php
@@ -2,36 +2,54 @@
 
 namespace SchemaTransformer\LockRunner;
 
+use Override;
 use PHPUnit\Framework\Attributes\TestDox;
+use Stringable;
 
 class LockRunnerTest extends \PHPUnit\Framework\TestCase
 {
     #[TestDox('does not allow multiple instances of the same lock to run concurrently')]
     public function testLockRunner()
     {
-        $id         = 'test-lock-runner';
-        $lockRunner = new LockRunner($id);
+        $logger = static::getLogger();
+        $id     = 'test-lock-runner';
+
+        $lockRunner = new LockRunner($id, $logger);
         $lockRunner->lock();
 
-        $lockRunner2 = new LockRunner($id);
-        $this->expectException(LockRunnerException::class);
-        $this->expectExceptionMessage('Lock already acquired for ' . $id);
-
+        $lockRunner2 = new LockRunner($id, $logger);
         $lockRunner2->lock();
+
+        static::assertCount(1, $logger->logMessages);
+        static::assertSame($logger->logMessages[0]['level'], 'warning');
+        static::assertSame($logger->logMessages[0]['message'], 'Lock already acquired for ' . $id);
     }
 
     #[TestDox('allows reuse of lock if the lock is released')]
     public function testLockRunnerRelease()
     {
+        $logger     = static::getLogger();
         $id         = 'test-lock-runner-release';
-        $lockRunner = new LockRunner($id);
+        $lockRunner = new LockRunner($id, $logger);
         $lockRunner->lock();
         $lockRunner->release();
 
-        $lockRunner2 = new LockRunner($id);
+        $lockRunner2 = new LockRunner($id, $logger);
         $lockRunner2->lock();
         $lockRunner2->release();
 
-        static::assertTrue(true, 'Lock was successfully acquired and released without exceptions.');
+        static::assertCount(0, $logger->logMessages);
+    }
+
+    private static function getLogger(): \Psr\Log\LoggerInterface
+    {
+        return new class extends \SchemaTransformer\Loggers\NullLogger {
+            public array $logMessages = [];
+
+            public function warning(string|Stringable $message, array $context = []): void
+            {
+                $this->logMessages[] = ['level' => 'warning', 'message' => (string)$message, 'context' => $context];
+            }
+        };
     }
 }
