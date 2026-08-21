e48ad5d56c0701d335687ff81e2d3038196e8fe1
Thor Brink <thor.brink@helsingborg.se>
2026-08-21 06:54:00 +0000

    refactor: default to empty strings in TypesenseClientFactory

--- run/Factories/TypesenseClientFactory.php
+++ run/Factories/TypesenseClientFactory.php
@@ -7,12 +7,12 @@ class TypesenseClientFactory
     public static function create(): \Typesense\Client
     {
         return new \Typesense\Client([
-            'api_key' => getenv('TYPESENSE_API_KEY'),
+            'api_key' => getenv('TYPESENSE_API_KEY') ?? '',
             'nodes'   => [
                 [
-                    'host'     => getenv('TYPESENSE_HOST'),
-                    'port'     => (int)getenv('TYPESENSE_PORT'),
-                    'protocol' => getenv('TYPESENSE_PROTOCOL'),
+                    'host'     => getenv('TYPESENSE_HOST') ?? '',
+                    'port'     => (int)getenv('TYPESENSE_PORT') ?? '',
+                    'protocol' => getenv('TYPESENSE_PROTOCOL') ?? '',
                 ],
             ],
         ]);
